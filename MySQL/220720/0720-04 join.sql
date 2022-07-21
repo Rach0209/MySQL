d97b9e83b2167903cfce2828294ceae40355ef63
you_tu <104557592+Rach0209@users.noreply.github.com>
2022-07-21 10:44:26 +0900

    Add files via upload

--- MySQL/220720/0720-04 join.sql
+++ MySQL/220720/0720-04 join.sql
@@ -6,11 +6,12 @@ SELECT * FROM menus AS A
 RIGHT JOIN restaurants AS B ON A.restId = B.RestaurantId;
 
 
--- INNER JOIN : 값이 있는것만.
--- OUTER JOIN : 값 없는 것만
+-- INNER JOIN : 값이 있는것만. (결합이 되는 행들만 남고, 나머지는 제거 후 조회)
+-- LEFT OUTER JOIN : 왼쪽 기준으로 값 없는 것도 다 조회
 SELECT * FROM restaurants AS A
 -- 기준 : restaurants 20개 ;// 원래는 LEFT OUTER JOIN ; 줄여서 LEFT JOIN 
 LEFT JOIN menus AS B ON A.RestaurantId = B.restId;
 
+-- 기준인 오른쪽이 다 나옴. 참조가 없는 것들도 나온다.
 SELECT * FROM restaurants AS A
 RIGHT JOIN menus AS B ON a.RestaurantId = B.RestId;
--- MySQL/220720/0720-06 test.sql
+++ MySQL/220720/0720-06 test.sql
@@ -11,30 +11,37 @@ SELECT * FROM hr.employees;
     4. 직원의 해당 직책의 jobs테이블에 책정된 최대 연봉(MAX_SALARY)을 초과한 연봉을 받는 직원이름, 현재연봉, 최대연봉 조회
     5. 부서의 (COUNTRY_ID)가 'US'인 부서에서 근무하는 직원명과 부서명.
 */
+use hr;
 SELECT * FROM departments;
 select * from jobs;
 select * from locations;
 -- 1.
 SELECT A.DEPARTMENT_NAME, B.FIRST_NAME, B.LAST_NAME
 FROM departments AS A
-JOIN employees AS B ON A.MANAGER_ID = B.MANAGER_ID;
+LEFT OUTER JOIN employees AS B ON A.MANAGER_ID = B.EMPLOYEE_ID;
+
 
 -- 2.
 SELECT A.FIRST_NAME, A.LAST_NAME, B.DEPARTMENT_NAME FROM employees AS A
-RIGHT JOIN departments AS B ON B.DEPARTMENT_ID = A.DEPARTMENT_ID
+JOIN departments AS B ON B.DEPARTMENT_ID = A.DEPARTMENT_ID
 WHERE B.DEPARTMENT_NAME LIKE 'IT%';
 
 -- 3.
 SELECT A.FIRST_NAME, A.LAST_NAME, B.JOB_TITLE FROM employees AS A
 JOIN jobs AS B ON B.JOB_ID = A.JOB_ID;
 
--- 4.
+-- 4. 직원의 해당 직책의 jobs테이블에 책정된 최대 연봉(MAX_SALARY)을 초과한 연봉을 받는 직원이름, 현재연봉, 최대연봉 조회
 SELECT A.FIRST_NAME, A.SALARY, B.MAX_SALARY FROM employees AS A
 JOIN jobs AS B ON B.JOB_ID = A.JOB_ID
 WHERE A.SALARY >= B.MAX_SALARY;
 
--- 5.
+-- 5. 부서의 (COUNTRY_ID)가 'US'인 부서에서 근무하는 직원명과 부서명.
 SELECT A.FIRST_NAME, A.LAST_NAME, B.DEPARTMENT_NAME FROM employees AS A
 JOIN departments AS B ON B.DEPARTMENT_ID = A.DEPARTMENT_ID
 JOIN locations AS C ON C.LOCATION_ID = B.LOCATION_ID
 WHERE C.COUNTRY_ID = 'US';
+
+SELECT * FROM locations A 
+JOIN departments B ON A.LOCATION_ID = B.LOCATION_ID
+JOIN employees C ON B.DEPARTMENT_ID = C.DEPARTMENT_ID
+WHERE COUNTRY_ID = 'US';
